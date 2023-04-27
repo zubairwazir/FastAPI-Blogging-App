cda0373198ec936ca1a41b67e40e5c1fefa4c4b5
zubairwazir <zubairwazir777@gmail.com>
2023-04-27 22:24:19 +0500

    User Models created

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from offline.database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+
+class UserModel(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    fname = Column(String)
+    lname = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+    age = Column(Integer)
+    is_active = Column(Boolean, default=False)
+    is_admin = Column(Boolean, default=False)
+
+    def __repr__(self):
+        return f"<User {self.email}"
