6a42b7bf86f75715f4531b17918b65df9e93e1e4
zubairwazir <zubairwazir777@gmail.com>
2023-04-28 20:29:42 +0500

    post route to create blog created

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi_offline import FastAPIOffline as FastAPI
+from typing import List
 from fastapi import Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
-from app.models import UserModel, Base
+from app.config import HashPwd
 from app.database import engine, get_db
-from app.schemas import UserSchema, UserUpdateSchema, UserLoginSchema
+from app.models import Base, UserModel, BlogModel
+from app.schemas import UserSchema, UserUpdateSchema, UserLoginSchema, UserResponseSchema, BlogSchema, BlogUpdateSchema, BlogResponseSchema
 
 Base.metadata.create_all(bind=engine)
 
@@ -17,7 +19,7 @@ async def index():
 
 @app.post("/register_user", status_code=status.HTTP_201_CREATED)
 def register_user(user: UserSchema, db: Session = Depends(get_db)):
-    new_user = UserModel(fname=user.fname, lname=user.lname, email=user.email, password=user.password,
+    new_user = UserModel(fname=user.fname, lname=user.lname, email=user.email, password=HashPwd.bcrypt(user.password),
                          age=user.age, is_active=user.is_active, is_admin=user.is_admin)
 
     if db.query(UserModel).filter(UserModel.email == user.email).first():
@@ -29,18 +31,15 @@ def register_user(user: UserSchema, db: Session = Depends(get_db)):
     return new_user
 
 
-@app.get("/get_all_users", status_code=status.HTTP_200_OK)
-def get_all_users(db: Session = Depends(get_db)):
-    users = db.query(UserModel).all()
-    if not users:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No Users found!")
-    return users
+@app.post("/login_user")
+def login_user(user: UserLoginSchema, db: Session = Depends(get_db)):
+    return {"User": "Logged In"}
 
 
-@app.get("/get_user/{id}", status_code=status.HTTP_200_OK)
+@app.get("/get_user/{id}", status_code=status.HTTP_200_OK, response_model=UserResponseSchema)
 def get_user(id: int, response: Response, db: Session = Depends(get_db)):
-    user = db.query(UserModel).filter(UserModel.id == id).first()
-    if not user:
+    user = db.query(UserModel).filter(UserModel.id == id)
+    if not user.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id {id} is not exists")
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {"detail": f"user with id {id} is not exists"}
@@ -68,6 +67,38 @@ def delete_user(id: int, db: Session = Depends(get_db)):
     return {"detail": f"User with id {id} is deleted!"}
 
 
-@app.post("/login_user")
-def login_user(user: UserLoginSchema, db: Session = Depends(get_db)):
-    return {"User": "Logged In"}
+@app.get("/get_all_users", status_code=status.HTTP_200_OK, response_model=List[UserResponseSchema])
+def get_all_users(db: Session = Depends(get_db)):
+    users = db.query(UserModel).all()
+    if not users:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No Users found!")
+    return users
+
+
+@app.post("/create_blog", status_code=status.HTTP_201_CREATED, response_model=BlogResponseSchema)
+def create_blog(blog: BlogSchema, db: Session = Depends(get_db)):
+    new_blog = BlogModel(title=blog.title, description=blog.description)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get("/get_blog/{id}", status_code=status.HTTP_200_OK, response_model=BlogResponseSchema)
+def get_blog(id: int, db: Session = Depends(get_db)):
+    blog = db.query(BlogModel).filter(BlogModel.id == id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No Blog with id {id} Found!")
+
+    return blog
+
+
+@app.get("/get_blogs", status_code=status.HTTP_200_OK, response_model=BlogResponseSchema)
+def get_blog(id: int, db: Session = Depends(get_db)):
+    blogs = db.query(BlogModel).all()
+
+    if not blogs:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No Blog Found!")
+
+    return blogs
