c3b233e16c0c2510d9364f2c957aead99c50a8c3
zubairwazir <zubairwazir777@gmail.com>
2023-04-29 23:05:01 +0500

    auth router registered

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi_offline import FastAPIOffline as FastAPI
 from app.config.database import engine
 from app.models.models import Base
-from app.routers import user, blog
+from app.routers import user, blog, auth
 
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(blog.router)
