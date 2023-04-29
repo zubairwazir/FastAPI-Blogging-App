e106ea287837abb3f0684a63824e08fbff97d908
zubairwazir <zubairwazir777@gmail.com>
2023-04-29 23:08:17 +0500

    import statement -hashpwd renamed to HashPassowrd

--- app/repository/user.py
+++ app/repository/user.py
@@ -1,14 +1,24 @@
-from fastapi import APIRouter, Depends, status, HTTPException
-from sqlalchemy.orm import Session
-from typing import List
-from app.config.database import get_db
-from app.config.hashing import HashPwd
+from fastapi import status, HTTPException
+from app.config.hashing import HashPassword
 from app.models.models import UserModel
-from app.schemas.schemas import UserSchema, UserUpdateSchema, UserLoginSchema, UserResponseSchema
 
 
-def create_user(user: UserSchema, db: Session = Depends(get_db)):
-    new_user = UserModel(fname=user.fname, lname=user.lname, email=user.email, password=HashPwd.bcrypt(user.password),
+def get(id, db):
+    user = db.query(UserModel).filter(UserModel.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id {id} is not exists")
+    return user
+
+
+def get_all(db):
+    users = db.query(UserModel).all()
+    if not users:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No Users found!")
+    return users
+
+
+def create(user, db):
+    new_user = UserModel(fname=user.fname, lname=user.lname, email=user.email, password=HashPassword.bcrypt(user.password),
                          age=user.age, is_active=user.is_active, is_admin=user.is_admin)
 
     if db.query(UserModel).filter(UserModel.email == user.email).first():
@@ -20,8 +30,25 @@ def create_user(user: UserSchema, db: Session = Depends(get_db)):
     return new_user
 
 
-def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(UserModel).filter(UserModel.id == id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id {id} is not exists")
-    return user
+def update(id, user, db):
+    get_user = get(id, db)
+    if not get_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id{id} not exists!")
+
+    get_user.update(user)
+    db.commit()
+    return {"detail": f"User with id {id} is updated!"}
+
+
+def delete(id, db):
+    get_user = get(id, db)
+    if not get_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User with id {id} not Found!")
+
+    get_user.delete(synchronize_session=False)
+    db.commit()
+    return {"detail": f"User with id {id} is deleted!"}
+
+
+def login(db):
+    pass
