078186deb28e8d10ecc8e55f2a1d64059380e18e
zubairwazir <zubairwazir777@gmail.com>
2023-04-29 23:05:30 +0500

    verify password function added

--- app/config/hashing.py
+++ app/config/hashing.py
@@ -3,6 +3,9 @@ from passlib.context import CryptContext
 pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-class HashPwd:
+class HashPassword:
     def bcrypt(self: str):
         return pwd_cxt.hash(self)
+
+    def verify(plain_passowrd, hashed_password):
+        return pwd_cxt.verify(plain_passowrd, hashed_password)
