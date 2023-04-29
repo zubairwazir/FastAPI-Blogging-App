2c89ee68e08113cdd34ff77ef514d2eb8285d5b0
zubairwazir <zubairwazir777@gmail.com>
2023-04-29 20:45:02 +0500

    model refactored

--- app/models/models.py
+++ app/models/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
-from app.database import Base
+from app.config.database import Base
 
 
 class UserModel(Base):
