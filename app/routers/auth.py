fc4d6f27e266f7f08579442acbd65f54e2d90551
zubairwazir <zubairwazir777@gmail.com>
2023-04-29 23:05:57 +0500

    login router added

---
+++ app/routers/auth.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from app.config.database import get_db
+from app.config.hashing import HashPassword
+from app.models.models import UserModel
+from app.schemas.schemas import UserLoginSchema
+
+
+router = APIRouter(
+    tags=['Auth']
+)
+
+
+@router.post('/login')
+def login(user: UserLoginSchema, db: Session = Depends(get_db)):
+    get_user = db.query(UserModel).filter(UserModel.email == user.username).first()
+
+    if not get_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+
+    if not HashPassword.verify(user.password, get_user.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Incorrect Password")
+
+    return get_user
