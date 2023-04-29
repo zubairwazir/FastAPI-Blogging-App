8af9ed62576ede55e3fe84baefa242d5298062af
zubairwazir <zubairwazir777@gmail.com>
2023-04-29 18:53:20 +0500

    config modules created -app refactored

--- app/config.py
+++
@@ -1,8 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-class HashPwd:
-    def bcrypt(self: str):
-        return pwd_cxt.hash(self)
